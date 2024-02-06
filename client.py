984b2330a081692a6095c33667be0ed4544596e1
Omkar Phansopkar <omkarphansopkar@gmail.com>
2024-02-06 22:29:35 +0530

    CLI options for rpc client
    Signed-off-by: Omkar Phansopkar <omkarphansopkar@gmail.com>

--- client.py
+++ client.py
@@ -1,3 +1,4 @@
+import argparse
 import grpc
 from rpc import patient_service_pb2
 from rpc import patient_service_pb2_grpc
@@ -46,20 +47,67 @@ class PatientClient:
         response = self.stub.update_patient(patient_with_id)
         return response
 
-if __name__ == '__main__':
+def main():
+    parser = argparse.ArgumentParser(description='Perform actions on patient service.')
+    parser.add_argument('--server', type=str, default='localhost:50051', help='Server address')
+    parser.add_argument('--action', type=str, required=True, choices=['add', 'get', 'update', 'list'], help='Action to perform (add, get, update, list)')
+    parser.add_argument('--patient_id', type=int, help='Patient ID (required for get and update actions)')
+    parser.add_argument('--name', type=str, help='Name of the patient')
+    parser.add_argument('--age', type=int, help='Age of the patient')
+    parser.add_argument('--gender', type=str, help='Gender of the patient')
+    parser.add_argument('--blood_pressure', type=str, help='Blood pressure of the patient')
+    parser.add_argument('--diabetes_level', type=float, help='Diabetes level of the patient')
+    parser.add_argument('--blood_group', type=str, help='Blood group of the patient')
+    parser.add_argument('--height', type=float, help='Height of the patient')
+    parser.add_argument('--weight', type=float, help='Weight of the patient')
+    args = parser.parse_args()
+
+    # print(args)
+
     client = PatientClient('localhost:50051')
+
+    if args.action == 'add':
+        response = client.add_patient(args.name, args.age, args.gender, args.blood_pressure, args.diabetes_level, args.blood_group, args.height, args.weight)
+        print("Added Patient:\n", response)
     
-    response = client.add_patient("John Doe", 30, "Male", "120/80", 6.5, "A+", 180.0, 75.0)
-    print("Added Patient:", response)
+    elif args.action == 'get':
+        if args.patient_id is None:
+            print("Please provide patient ID.")
+            return
+        try:
+            response = client.get_patient_by_id(args.patient_id)
+            print(f"Patient with ID {args.patient_id}:\n", response)
+        except:
+            print("Patient not found")
+
+    elif args.action == 'update':
+        if args.patient_id is None:
+            print("Please provide patient ID.")
+            return
+        response = client.update_patient(args.patient_id, args.name, args.age, args.gender, args.blood_pressure, args.diabetes_level, args.blood_group, args.height, args.weight)
+        print("Updated Patient:\n", response)
+
+    elif args.action == 'list':
+        print("All Patients:")
+        for patient in client.get_all_patients():
+            print(patient)
     
-    patient_id = 1
-    response = client.get_patient_by_id(patient_id)
-    print(f"Patient with ID {patient_id}:\n", response)
+    # response = client.add_patient("John Doe", 30, "Male", "120/80", 6.5, "A+", 180.0, 75.0)
+    # print("Added Patient:", response)
+    
+    # patient_id = 1
+    # response = client.get_patient_by_id(patient_id)
+    # print(f"Patient with ID {patient_id}:\n", response)
 
-    response = client.update_patient(1, "Jane Doe", 39, "Male", "120/80", 6.5, "A+", 180.0, 75.0)
-    print("Updated Patient:", response)
+    # response = client.update_patient(1, "Jane Doe", 39, "Male", "120/80", 6.5, "A+", 180.0, 75.0)
+    # print("Updated Patient:", response)
 
 
-    print("All Patients:")
-    for patient in client.get_all_patients():
-        print(patient)
+    # print("All Patients:")
+    # for patient in client.get_all_patients():
+    #     print(patient)
+            
+
+
+if __name__ == '__main__':
+    main()
